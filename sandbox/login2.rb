a6a52e42af9013bc6f6c057af0ecead2f3f82d34
eternal golden bread <codylewisbrock@gmail.com>
2019-12-28 10:06:30 -0800

    finishes work on login.  now uses include to put the module directly in class

--- sandbox/login2.rb
+++ sandbox/login2.rb
@@ -1,12 +1,29 @@
 require_relative 'login'
 
-users = [
-          { username: "mashrur", password: "password1" },
-          { username: "jack", password: "password2" },
-          { username: "arya", password: "password3" },
-          { username: "jonshow", password: "password4" },
-          { username: "heisenberg", password: "password5" }
-        ]
-
-hashed_users = Login.create_secure_users(users)
-puts hashed_users
+class Student
+  include Login
+  attr_accessor :first_name, :last_name, :email, :username, :password
+ 
+  def initialize(firstname, lastname, username, email, password)
+    @first_name = firstname
+    @last_name = lastname
+    @username = username
+    @email = email
+    @password = password
+  end
+ 
+  def to_s
+    "First name: #{@first_name}, Last name: #{@last_name}, Username: #{@username},
+                  email address: #{@email}"
+  end
+ 
+end
+ 
+cody = Student.new("Cody", "Brock", "cody1", "cody@example.com",
+                      "password10000")
+john = Student.new("John", "Doe", "john1", "john1@example.com",
+                      "password2")
+ 
+hashed_password = cody.create_hash_digest(cody.password)
+ 
+puts hashed_password
